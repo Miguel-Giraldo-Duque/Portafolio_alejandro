5c2b0abdc5b1760b380ae20d291950e2d73aa2cf
Miguel Angel <miguelgiradu@gmail.com>
2023-10-06 12:34:35 -0500

    Prueba movil

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
 const openModal = document.querySelectorAll("[data-target]")
 const modal = document.querySelectorAll(".modal2")
 const closeModal = document.querySelectorAll(".close-modal")
+const source = document.createElement("source")
+const video = document.getElementById("video")
 
-console.log("Hola")
+
+function changeUrl(url) {
+    video.src = url
+    video.play()
+}
+
+if (screen.width < 1024) {
+    changeUrl(".")
+   console.log(video.src)
+}
+
+
+
+
+
+console.log(video)
 
 openModal.forEach(function(btn) {
    btn.addEventListener("click", ()=>{  
@@ -22,7 +39,7 @@ document.addEventListener("DOMContentLoaded",() =>{
     const elementosCarrusel = document.querySelectorAll(".carousel")
    let date = M.Carousel.init(elementosCarrusel ,{
         duration: 150,
-       
+        indicators: true ,
         paddin: 5,
         numVisible: 2
     })
